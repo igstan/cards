e81292e7ec5fad1cb70fcca347e4ec52d88791ff
Ionuț G. Stan <ionut.g.stan@gmail.com>
2015-05-11 14:36:57 +0300

    Test both ordered and shuffled card iterators

--- src/test/scala/CardIteratorTest.scala
+++ src/test/scala/CardIteratorTest.scala
@@ -1,38 +1,76 @@
 package cards
 package test
 
-class CardIteratorTest extends FunSuite with Matchers {
-  def suits = {
-    CardIterator.ordered.toList.groupBy(_.suit)
-  }
+class CardIteratorTest extends FunSpec with Matchers {
+  describe("an ordered iterator") {
+    def suits = {
+      CardIterator.ordered.toList.groupBy(_.suit)
+    }
 
-  test("produces 52 cards") {
-    CardIterator.ordered.length should be(52)
-  }
+    it("produces 52 cards") {
+      CardIterator.ordered.length should be(52)
+    }
 
-  test("produces all suits") {
-    suits.keys should be(Set(Spades, Diamonds, Hearts, Clubs))
-  }
+    it("produces all suits") {
+      suits.keys should be(Set(Spades, Diamonds, Hearts, Clubs))
+    }
 
-  test("produces 13 cards for each suit") {
-    Set(Spades, Diamonds, Hearts, Clubs).foreach { suit =>
-      suits(suit).length should be(13)
+    it("produces 13 cards for each suit") {
+      Set(Spades, Diamonds, Hearts, Clubs).foreach { suit =>
+        suits(suit).length should be(13)
+      }
+    }
+
+    it("spades respect value range") {
+      suits(Spades).map(_.value).toSet should be(1.to(13).toSet)
+    }
+
+    it("diamonds respect value range") {
+      suits(Diamonds).map(_.value).toSet should be(1.to(13).toSet)
+    }
+
+    it("hearts respect value range") {
+      suits(Hearts).map(_.value).toSet should be(1.to(13).toSet)
+    }
+
+    it("clubs respect value range") {
+      suits(Clubs).map(_.value).toSet should be(1.to(13).toSet)
     }
   }
 
-  test("spades respect value range") {
-    suits(Spades).map(_.value).toSet should be(1.to(13).toSet)
-  }
+  describe("a shuffled iterator") {
+    def suits = {
+      CardIterator.shuffled.toList.groupBy(_.suit)
+    }
 
-  test("diamonds respect value range") {
-    suits(Diamonds).map(_.value).toSet should be(1.to(13).toSet)
-  }
+    it("produces 52 cards") {
+      CardIterator.shuffled.length should be(52)
+    }
 
-  test("hearts respect value range") {
-    suits(Hearts).map(_.value).toSet should be(1.to(13).toSet)
-  }
+    it("produces all suits") {
+      suits.keys should be(Set(Spades, Diamonds, Hearts, Clubs))
+    }
 
-  test("clubs respect value range") {
-    suits(Clubs).map(_.value).toSet should be(1.to(13).toSet)
+    it("produces 13 cards for each suit") {
+      Set(Spades, Diamonds, Hearts, Clubs).foreach { suit =>
+        suits(suit).length should be(13)
+      }
+    }
+
+    it("spades respect value range") {
+      suits(Spades).map(_.value).toSet should be(1.to(13).toSet)
+    }
+
+    it("diamonds respect value range") {
+      suits(Diamonds).map(_.value).toSet should be(1.to(13).toSet)
+    }
+
+    it("hearts respect value range") {
+      suits(Hearts).map(_.value).toSet should be(1.to(13).toSet)
+    }
+
+    it("clubs respect value range") {
+      suits(Clubs).map(_.value).toSet should be(1.to(13).toSet)
+    }
   }
 }
--- src/test/scala/package.scala
+++ src/test/scala/package.scala
@@ -1,6 +1,7 @@
 package cards
 
 package object test {
+  type FunSpec =  org.scalatest.FunSpec
   type FunSuite = org.scalatest.FunSuite
   type Matchers = org.scalatest.Matchers
 }
